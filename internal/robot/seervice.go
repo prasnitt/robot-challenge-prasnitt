b2e315255d63404e56e329f88963472c7e7eed89
Prashant Agrawal <prashant.agrawal@2degrees.nz>
2025-07-19 05:20:47 +1200

    taken care of PR comments

--- internal/robot/seervice.go
+++ internal/robot/seervice.go
@@ -24,7 +24,7 @@ type RobotService interface {
 }
 
 type Service struct {
-	mu        sync.RWMutex    `json:"-"` // Mutex for concurrent access
+	mu        sync.RWMutex    // Mutex for concurrent access
 	ctx       context.Context // Context for cancellation
 	state     ServiceState    // Current state of the robot service
 	taskQueue chan RobotTask  // Channel for incoming tasks
@@ -39,6 +39,7 @@ func NewService(ctx context.Context, taskQueue chan RobotTask) *Service {
 	}
 }
 
+// Start begins processing tasks from the task queue.
 func (s *Service) Start() {
 	log.Println("Robot Service Started...")
 
@@ -77,7 +78,6 @@ func (s *Service) EnqueueTask(commands string, delayBetweenCommands string) (str
 	return task.ID, nil
 }
 
-// TODO: Implement task cancellation logic
 func (s *Service) CancelTask(taskID string) error {
 	taskState, err := s.GetTaskState(taskID)
 	if err != nil {
@@ -109,7 +109,7 @@ func (s *Service) HandleTask(task RobotTask) {
 	log.Printf("Processing task %s with commands: %s", task.ID, task.Commands)
 	for _, cmd := range task.Commands {
 
-		// Make sure if the task is cancelled, we stop processing
+		// Make sure if the task is requested for cancellation, we stop processing
 		state, err := s.GetTaskState(task.ID)
 		if err != nil {
 			log.Printf("Error getting task state for %s: %v", task.ID, err)
@@ -122,8 +122,19 @@ func (s *Service) HandleTask(task RobotTask) {
 			return // Stop processing the task
 		}
 
+		time.Sleep(time.Duration(task.DelayBetweenCommands)) // Simulate delay between commands
+
 		// Execute each command in the task
-		s.ExecuteRobotCommand(cmd, task.DelayBetweenCommands) // Execute each command in the task
+		err = s.ExecuteRobotCommand(cmd, task.DelayBetweenCommands) // Execute each command in the task
+
+		if err != nil {
+			log.Printf("Error executing command '%s' for task %s: %v", cmd, task.ID, err)
+			s.UpdateTaskState(task.ID, Aborted) // Update the task state to Aborted
+			return                              // Stop processing the task on error
+		}
+
+		robotState := s.GetRobotState() // Get the current robot state after executing the command
+		log.Printf("Command '%s' Executed Robot moved to position: (%d, %d)", cmd, robotState.X, robotState.Y)
 	}
 
 	// Update the task state to Completed
@@ -132,25 +143,34 @@ func (s *Service) HandleTask(task RobotTask) {
 }
 
 // Execute a robot command and update the robot's position
-func (s *Service) ExecuteRobotCommand(cmd RobotCommand, durationBetweenCmds CommandDuration) {
+func (s *Service) ExecuteRobotCommand(cmd RobotCommand, durationBetweenCmds CommandDuration) error {
 
 	robotState := s.GetRobotState() // Get the current robot state
 	switch cmd {
 	case North:
+		if robotState.Y >= warehouseSize {
+			return fmt.Errorf("robot cannot move north, out of warehouse boundaries")
+		}
 		robotState.Y++
 	case South:
+		if robotState.Y <= 0 {
+			return fmt.Errorf("robot cannot move south, out of warehouse boundaries")
+		}
 		robotState.Y--
 	case East:
+		if robotState.X >= warehouseSize {
+			return fmt.Errorf("robot cannot move east, out of warehouse boundaries")
+		}
 		robotState.X++
 	case West:
+		if robotState.X <= 0 {
+			return fmt.Errorf("robot cannot move west, out of warehouse boundaries")
+		}
 		robotState.X--
 	}
 
 	s.SetRobotState(robotState) // Update the robot state in the service
-
-	time.Sleep(time.Duration(durationBetweenCmds)) // Simulate delay between commands
-
-	log.Printf("Command '%s' Executed Robot moved to position: (%d, %d)", cmd, robotState.X, robotState.Y)
+	return nil
 }
 
 func (s *Service) GetTaskState(taskID string) (TaskState, error) {
@@ -163,6 +183,8 @@ func (s *Service) GetTaskState(taskID string) (TaskState, error) {
 }
 
 func (s *Service) UpdateTaskState(taskID string, state TaskState) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
 	if task, exists := s.state.Tasks[taskID]; exists {
 		task.State = state
